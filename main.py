d4d993b5463bb47a388c70d0a7c6664deab26500
maguro923 <maguro29y@gmail.com>
2024-08-09 14:52:48 +0000

    feat: 試験的なwebsocketエンドポイントを作成
    試験的にwebsocketエンドポイントを実装しました
    websocket通信中のユーザーidやアクセストークンの期限確認、受信したメッセージの形式確認やエラーハンドリングなどの基本的な部分のみの実装で、実際の仕様とは異なる試験的なものであることに留意してください

--- main.py
+++ main.py
@@ -1,86 +1,14 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-from routers import login, register, delete, refresh
-from typing import List
+from fastapi import FastAPI
+from routers import login, register, delete, refresh, websocket
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://" + window.location.host + "/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-
-"""
-"""
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-@app.websocket("/ws")
-async def websocket_endpoint(ws: WebSocket):
-    await manager.connect(ws)
-    try:
-        while True:
-            data = await ws.receive_text()
-            await ws.send_text(f"Message text was: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(ws)
-        await manager.broadcast(f"Client left the chat")
-"""
-
 @app.exception_handler(RequestValidationError)
 def validation_handler(request, exc):
+    print(request)
+    print(exc)
     error_lists = []
     for e in exc.errors():#エラーメッセージの抽出
         if 'ctx' in e and 'error' in e['ctx']:
@@ -110,3 +38,4 @@ app.include_router(login.router)
 app.include_router(register.router)
 app.include_router(delete.router)
 app.include_router(refresh.router)
+app.include_router(websocket.router)
---
+++ routers/websocket.py
@@ -0,0 +1,103 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import List, Dict
+import asyncio
+import json
+from config import VALIDITY_HOURS
+from datetime import datetime, timedelta
+import pytz
+
+router = APIRouter()
+
+class ConnectionManager:
+    """
+    websocketの接続管理を行う
+    """
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+        self.active_users_id: List[str] = []
+        self.latest_token_valid: Dict[str, datetime] = {}
+        self.lock = asyncio.Lock()
+
+    async def connect(self, websocket: WebSocket, user_id: str):
+        await websocket.accept()
+        async with self.lock:
+            self.active_connections.append(websocket)
+            self.active_users_id.append(user_id)
+            self.latest_token_valid[user_id] = pytz.timezone('Asia/Tokyo').localize(datetime.now())+timedelta(hours=9)
+        print(self.active_users_id)
+
+    async def disconnect(self, websocket: WebSocket, user_id: str):
+        print("disconnect")
+        async with self.lock:
+            self.active_connections.remove(websocket)
+            self.active_users_id.remove(user_id)
+            del self.latest_token_valid[user_id]
+        await manager.broadcast(f"Client left the chat")
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_json(message)
+
+    async def broadcast(self, message: str, websocket=None):
+        for connection in self.active_connections:
+            if websocket != None and connection == websocket:
+                continue
+            await connection.send_json(message)
+
+
+manager = ConnectionManager()
+
+async def check_token(ws: WebSocket, user_id: str):
+    """
+    アクセストークンの有効期限を確認し、有効期限が切れた場合に切断する
+    """
+    while True:
+        print(f"Token check: {user_id}", manager.latest_token_valid[user_id],"{}h".format(VALIDITY_HOURS["access_token"]))
+        await asyncio.sleep(VALIDITY_HOURS["access_token"]*3600-600)
+        await manager.send_personal_message({"message":"Your access token has expired after 10 minutes. Please refresh access token."},ws)
+        await asyncio.sleep(600)
+        if pytz.timezone('Asia/Tokyo').localize(datetime.now())+timedelta(hours=9) > manager.latest_token_valid[user_id] + timedelta(hours=VALIDITY_HOURS["access_token"]):
+            print(f"Token expired: {user_id}")
+            raise WebSocketDisconnect
+
+
+async def send_msg(ws: WebSocket, user_id: str):
+    """
+    メッセージの送信を行う
+    """
+    while True:
+        message = {"message":f"Hello, {user_id}!","join_user":manager.active_users_id}
+        await manager.send_personal_message(message, ws)
+        await asyncio.sleep(360)
+
+async def recv_msg(ws: WebSocket, user_id: str):
+    """
+    メッセージの受信及び形式の確認等を行う
+    """
+    while True:
+        try:
+            print(f"waitng for message from {user_id}")
+            data = await ws.receive_text()
+            data = json.loads(data)
+            print(f"received message from {user_id}:",data)
+            message = {"message":data,"join_user":manager.active_users_id}
+            await manager.send_personal_message(message, ws)
+        except WebSocketDisconnect:
+            raise WebSocketDisconnect
+        except json.decoder.JSONDecodeError as e:
+            print("Error: Json decode error")
+            pass
+        except Exception as e:
+            print(f"Error: {e}",type(e))
+            return
+
+@router.websocket("/ws/{user_id}")
+async def websocket_endpoint(ws: WebSocket, user_id: str):
+    await manager.connect(ws, user_id)
+    try:
+        CheckToken = asyncio.create_task(check_token(ws, user_id))
+        send = asyncio.create_task(recv_msg(ws, user_id))
+        recv = asyncio.create_task(send_msg(ws, user_id))
+        await asyncio.gather(CheckToken, send, recv)
+    except WebSocketDisconnect:
+        await manager.disconnect(ws, user_id)
+        await manager.broadcast(f"Client left the chat")
